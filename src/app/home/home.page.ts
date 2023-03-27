e02ddd44eb3acd8cd1ba42adb315df9fa92827da
timmy forde <timmyforde02@gmail.com>
2023-03-27 10:40:26 +0100

    lab8

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { MovieService } from '../services/movie.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+movies:any[]=[];
 
-  constructor() {}
+ngOnInit(){
+  this.service.GetMovieData().subscribe(data=>
+ {
+    this.movies = data.Search;
+  });
+}
+  constructor(private service:MovieService) {}
 
 }
